4a100b22e658e8cd761cc922d7088cc4688b0b6a
Oleg Z <oleh.zhurylo@nure.ua>
2016-10-14 15:39:58 +0300

    Added some models

---
+++ SeaBattle/SeaBattle/Models/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle.Models
+{
+    class Player
+    {
+        String Name
+        {
+            get { return Name; }
+            set { Name = value; }
+        }
+        int Score
+        {
+            get { return Score; }
+            set { Score = value; }
+        }
+        int [,] Field
+        {
+            get { return Field; }
+            set { Field = value; }
+        }
+        List<Ship> Ships { get; set; }
+        public Player()
+        {
+            Field = new int[10, 10];
+            Ships = new List<Ship>();
+            Ships.Capacity = 10;
+        }
+    }
+}
---
+++ SeaBattle/SeaBattle/Models/TypesOfShips.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle.Models
+{
+    enum TypesOfShips { First, Second, Third, Fourth };
+}
