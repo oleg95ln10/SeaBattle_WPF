d4eb0cc0961970bef33d7f3a2b91e439fe8cfa98
Oleg Z <oleh.zhurylo@nure.ua>
2016-10-18 20:51:37 +0300

    Add half of vertical ship direction

--- SeaBattle/SeaBattle/View/PreGameWindow.xaml.cs
+++ SeaBattle/SeaBattle/View/PreGameWindow.xaml.cs
@@ -45,8 +45,8 @@ namespace SeaBattle.View
             Button but = sender as Button;
             var x = (int)( Canvas.GetLeft(but) / Cell.CellSize );
             var y = (int)( Canvas.GetTop(but) / Cell.CellSize );
-            lab1.Content = x;
-            shipDirection.Content = y;
+            lab1.Content = x+1;
+            shipDirection.Content = y+1;
             switch (_direction)
             {
                 case ShipDirection.Horizontal:
@@ -102,6 +102,71 @@ namespace SeaBattle.View
                     break;
 
                 case ShipDirection.Vertical:
+
+                    if ((x <= 10 && y + _currentShip <= 10) && (y + _currentShip >= 0 && x >= 0))
+                    {
+                        if (y - 1 >= 0 && x - 1 >= 0)
+                        {
+                            Button mostUpLeftChildButton = (Button)fieldController.canvas.Children[(x - 1) + (y - 1) * 10];
+                            //mostUpLeftChildButton.Background = Brushes.Yellow;
+                        }
+                        if (x + _currentShip <= 9 && y - 1 >= 0)
+                        {
+                            Button mostUpRightChildButton = (Button)fieldController.canvas.Children[(x + _currentShip) + (y - 1) * 10];
+                            //mostUpRightChildButton.Background = Brushes.Yellow;
+                        }
+                        if (x - 1 >= 0 && x - 1 <= 9)
+                        {
+                            Button mostMidleLeftChildButton = (Button)fieldController.canvas.Children[(x - 1) + y * 10];
+                            //mostMidleLeftChildButton.Background = Brushes.Yellow;
+                        }
+                        if (x + _currentShip <= 9)
+                        {
+                            Button mostMidleRightChildButton = (Button)fieldController.canvas.Children[(x + _currentShip) + y * 10];
+                            //mostMidleRightChildButton.Background = Brushes.Yellow;
+                        }
+                        if (y + 1 <= 9 && x - 1 >= 0)
+                        {
+                            Button mostDownLeftChildButton = (Button)fieldController.canvas.Children[(x - 1) + (y + 1) * 10];
+                            //mostDownLeftChildButton.Background = Brushes.Yellow;
+                        }
+                        if (x + _currentShip <= 9 && y + 1 < 9)
+                        {
+                            Button mostDownRightChildButton = (Button)fieldController.canvas.Children[(x + _currentShip) + (y + 1) * 10];
+                            //mostDownRightChildButton.Background = Brushes.Yellow;
+                        }
+                        for (int i = 0; i < _currentShip; ++i)
+                        {
+                            var numbOfShipCell = x + (y + i) * 10;
+                            Button childButton = (Button)fieldController.canvas.Children[numbOfShipCell];
+                            childButton.Background = Brushes.Red;
+                            if (numbOfShipCell - 1 >= 0)
+                            {
+                                Button leftChildButton = (Button)fieldController.canvas.Children[numbOfShipCell - 1];
+                                leftChildButton.Background = Brushes.Yellow;
+                            }
+                            if (numbOfShipCell + 1 <= 99)
+                            {
+                                Button belowerChildButton = (Button)fieldController.canvas.Children[numbOfShipCell + 1];
+                                belowerChildButton.Background = Brushes.Yellow;
+                            }
+                        }
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                     break;
             }
