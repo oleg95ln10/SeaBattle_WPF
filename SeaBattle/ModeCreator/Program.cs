c5fe000c38360e3ae36035a16f3d35438b24800a
Oleg <Oleg@OLEG_PC>
2016-10-23 22:56:11 +0300

    Created few player's patches

--- SeaBattle/ModeCreator/Program.cs
+++ SeaBattle/ModeCreator/Program.cs
@@ -8,8 +8,26 @@ namespace ModeCreator
 {
     class Program
     {
+        class MyField : ModeDLL.ShotMapCreator
+        {
+            public MyField(string fileToSave)
+                : base(fileToSave)
+            {
+
+            }
+
+            public override void GetShotMap()
+            {
+                for (int i = 99; i > -1; --i)
+                    _shotMap.Add(i);
+                base.WriteFieldToFile();
+            }
+
+        }
         static void Main(string[] args)
         {
+            MyField f = new MyField("endToStart.txt");
+            f.GetShotMap();
         }
     }
 }
